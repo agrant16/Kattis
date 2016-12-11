b3c8e3d32bbe80638f687832dd3497488abe7bc5
Alan Grant <kurfew007@gmail.com>
2016-12-11 14:37:56 -0500

    Add solution for Pig Latin

---
+++ PigLatin.py
@@ -0,0 +1,21 @@
+import sys
+
+vowels = 'aeiouy'
+
+def piglatin(word):
+    if word[0] in vowels:
+        word += 'yay'
+    else:
+        x = 0
+        new = ''
+        while word[x] not in vowels:
+            new += word[x]
+            x += 1
+        word = word[x:] + new + 'ay'
+    return word
+
+for x in sys.stdin:
+    line = x.split()
+    for i in range(len(line)):
+        line[i] = piglatin(line[i])
+    print(' '.join(line))
