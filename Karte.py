ead658a52c443ddc54a742fb3719aa941a1705df
Alan Grant <kurfew007@gmail.com>
2016-12-11 20:53:49 -0500

    Add solution for Karte

---
+++ Karte.py
@@ -0,0 +1,21 @@
+s = input()
+dup = False
+cards = {'P' : [0,0,0,0,0,0,0,0,0,0,0,0,0],
+         'H' : [0,0,0,0,0,0,0,0,0,0,0,0,0],
+         'T' : [0,0,0,0,0,0,0,0,0,0,0,0,0],
+         'K' : [0,0,0,0,0,0,0,0,0,0,0,0,0]}
+
+for x in range(0, len(s), 3):
+    key = s[x]
+    i = int(s[x+1:x+3]) - 1
+    cards[key][i] += 1
+    if cards[key][i] > 1:
+        print('GRESKA')
+        dup = True
+        break
+
+if not dup:
+    missing = [str(13 - sum(cards[x])) for x in 'PKHT']
+    print(' '.join(missing))
+    
+    
